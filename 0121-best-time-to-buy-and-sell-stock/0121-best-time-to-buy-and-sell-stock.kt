fc864fdd811a7227ee2c07855cb2b943f783ea1e
kumkumbadgujjar <kumkumbadgujjar@gmail.com>
2026-09-16 21:43:13 +0530

    Time: 5 ms (27.44%), Space: 65 MB (65.31%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var minPrice = prices[0]
+        var maxProfit = 0
+
+        for (i in 1 until prices.size ){
+            val profit = prices[i] - minPrice
+             maxProfit = maxOf(maxProfit, profit)
+              minPrice = minOf(minPrice, prices[i])
+
+
+        }
+
+         return maxProfit
+
+        
+    }
+}
